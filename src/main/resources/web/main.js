d20d2325df1c33c6fe339530cd3d702e3c6786a3
Steve Armstrong <steve@horsefire.com>
2013-07-01 00:22:09 -0700

    Got draggable zoomed image working

--- src/main/resources/web/main.js
+++ src/main/resources/web/main.js
@@ -27,19 +27,33 @@ var refresh = function() {
 		files.append(html);
 	}
 	
-	//openDoc('b987b2cb274a381637139acbd87fff0c6cf33853');
+	openDoc('b987b2cb274a381637139acbd87fff0c6cf33853');
 };
 
 var openDoc = function(id) {
 	var doc = docs[id];
 	var lightNode = $('#light');
 	lightNode.empty();
-	lightNode.append('<div id="full-image-viewport">'
-		+'<img id="full-image" src="/fetch?id='+id+'&type=thumb"/>'
-		+'</div>'
-		+'<span id="filename">'+doc.filename+'</span>'
-		+'<span id="uploaded">'+doc.uploaded+'</span>'
-		+'<span id="effective">'+doc.effective+'</span>');
+	
+	var html = '<div id="full-image-viewport">';
+	html += '<img id="full-image" src="/fetch?id='+id+'&type=thumb"/>';
+	html += '</div>';
+	html += '<div><span class="key">Filename</span><span class="value">'+doc.filename+'</span></div>';
+	html += '<div><span class="key">Uploaded</span><span class="value">'+doc.uploaded+'</span></div>';
+	html += '<div><span class="key">Effective</span><span class="value">'+doc.effective+'</span></div>';
+	lightNode.append(html);
+	
+	$('#full-image').draggable({
+		drag: function(event, ui) {
+			if(ui.position.top>0) { ui.position.top = 0; }
+			var maxtop = ui.helper.parent().height()-ui.helper.height();
+			if(ui.position.top<maxtop) { ui.position.top = maxtop; }
+			if(ui.position.left>0) { ui.position.left = 0; }
+			var maxleft = ui.helper.parent().width()-ui.helper.width();
+			if(ui.position.left<maxleft) { ui.position.left = maxleft; }
+		}
+	});
+	
 	var tagsInput = $('<input name="tags" id="tags" style="position:relative" value="'+doc.tags.join(',')+'"/>');
 	lightNode.append(tagsInput);
 	tagsInput.tagit();
