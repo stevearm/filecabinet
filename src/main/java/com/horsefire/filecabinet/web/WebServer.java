8efae6f2ba069aac5b17aad7a269c1440ea4881e
Steve Armstrong <steve@horsefire.com>
2013-07-13 11:18:10 -0700

    Limit Jetty to 6 threads, and name them properly

--- src/main/java/com/horsefire/filecabinet/web/WebServer.java
+++ src/main/java/com/horsefire/filecabinet/web/WebServer.java
@@ -1,13 +1,20 @@
 package com.horsefire.filecabinet.web;
 
 import java.util.EnumSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import javax.servlet.DispatcherType;
 
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.Inject;
 import com.google.inject.servlet.GuiceFilter;
 
@@ -17,7 +24,14 @@ public class WebServer {
 
 	@Inject
 	public WebServer() {
-		m_server = new Server(80);
+		ThreadFactory factory = new ThreadFactoryBuilder().setNameFormat(
+				"jetty-worker-%s").build();
+		ExecutorService threadPool = Executors.newFixedThreadPool(6, factory);
+		m_server = new Server(new ExecutorThreadPool(threadPool));
+
+		ServerConnector connector = new ServerConnector(m_server);
+		connector.setPort(80);
+		m_server.setConnectors(new Connector[] { connector });
 
 		ServletContextHandler handler = new ServletContextHandler(m_server,
 				"/", ServletContextHandler.NO_SECURITY
