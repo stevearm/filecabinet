0c208479ab7db1d5b34988dbfc70dcb2cbb9c483
Steve Armstrong <steve@horsefire.com>
2013-08-27 01:06:59 -0700

    Splitting document editing out into it's own file, and using jquery.couch.js to clean up code

---
+++ couchapp/_attachments/script/main.js
@@ -0,0 +1,25 @@
+var design, db;
+$(function() {
+	var path = unescape(document.location.pathname).split('/');
+	design = path[3];
+	db = $.couch.db(path[1]);
+	if (window.bootstrap) { window.bootstrap(); }
+});
+
+var dateArrayToString = function(dateArray) {
+	var dateString = "" + dateArray[0] + "-";
+	if (dateArray[1] < 10) {
+		dateString += "0";
+	}
+	dateString += dateArray[1] + "-";
+	if (dateArray[2] < 10) {
+		dateString += "0";
+	}
+	dateString += dateArray[2];
+	return dateString;
+};
+
+var dateStringToArray = function(dateString) {
+	var parts = dateString.split("-");
+	return [ parseInt(parts[0]), parseInt(parts[1]), parseInt(parts[2]) ];
+};
