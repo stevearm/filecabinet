21cb63f09c70399696cd6c6c4538bea3752a8e9e
Steve Armstrong <steve@horsefire.com>
2014-02-05 23:19:42 -0800

    Fixing header nav bar

--- couchapp/_attachments/js/app.js
+++ couchapp/_attachments/js/app.js
@@ -10,28 +10,32 @@ angular.module("filecabinet", [
     "filecabinet.directives",
     "filecabinet.factories",
     "filecabinet.services"
-]).
+])
 
-config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl:    'partials/index.html',
-        controller:     'IndexCtrl'
-    });
-    $routeProvider.when('/doc/:docId', {
-        templateUrl:    'partials/document.html',
-        controller:     'DocumentCtrl'
-    });
-    $routeProvider.when('/unseen', {
-        templateUrl:    'partials/unseen.html',
-        controller:     'UnseenCtrl'
-    });
-    $routeProvider.when('/unprocessed', {
-        templateUrl:    'partials/unseen.html',
-        controller:     'UnprocessedCtrl'
-    });
-    $routeProvider.when('/upload', {
-        templateUrl:    'partials/upload.html',
-        controller:     'UploadCtrl'
-    });
-    $routeProvider.otherwise({redirectTo: '/'});
-}]);
+.config([
+    "$routeProvider",
+    function($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl:    'partials/index.html',
+            controller:     'IndexCtrl'
+        })
+        .when('/doc/:docId', {
+            templateUrl:    'partials/document.html',
+            controller:     'DocumentCtrl'
+        })
+        .when('/unseen', {
+            templateUrl:    'partials/unseen.html',
+            controller:     'UnseenCtrl'
+        })
+        .when('/unprocessed', {
+            templateUrl:    'partials/unseen.html',
+            controller:     'UnprocessedCtrl'
+        })
+        .when('/upload', {
+            templateUrl:    'partials/upload.html',
+            controller:     'UploadCtrl'
+        })
+        .otherwise({redirectTo: '/'});
+    }
+]);
